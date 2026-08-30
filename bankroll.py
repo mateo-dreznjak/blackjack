caf8d914ed49549b20e96c6661daa2b39066cde4
mateo-dreznjak <mateo.dreznjak@gmx.de>
2026-08-30 15:07:37 +0200

    Remove dead placeBet method from bankroll

--- bankroll.py
+++ bankroll.py
@@ -3,17 +3,6 @@ class Bankroll:
     def __init__(self, starting_amount=500):
         self.balance = starting_amount
 
-
-    def placeBet(self, amount, hand):
-        while True:
-            if amount <= self.balance and amount % 25 == 0:
-                hand.bet += amount
-                self.balance.withDrawAmount(amount)
-                print(f"Your bet of {amount} has been placed!")
-                break
-            else:
-                amount = input("Enter a valid bet amount: ")
-
     def withdrawAmount(self, amount):
         self.balance -= amount
 
